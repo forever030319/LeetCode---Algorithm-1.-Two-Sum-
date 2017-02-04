b83b55b83522ca5570048088d03a4ab4ea352ea9
forever030319 <forever030319@gmail.com>
2017-02-04 14:17:06 +0800

    Update 9. Palindrome Number.cpp

--- 9. Palindrome Number.cpp
+++ 9. Palindrome Number.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0|| (x!=0 &&x%10==0)) return false;
+        if(x<0|| (x!=0 && x%10==0)) 
+            return false;
         int sum=0;
         while(x>sum)
         {
